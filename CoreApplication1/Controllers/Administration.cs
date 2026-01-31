77d4feb72eabf56ef65933d5a41429c4e422c3e4
jerom <jerom@LAPTOP-L0BU5SNG>
2026-01-31 16:27:03 -0500

    changes21

---
+++ CoreApplication1/Controllers/Administration.cs
@@ -0,0 +1,72 @@
+﻿using CoreApplication1.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApplication1.Controllers
+{
+    public class Administration : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public Administration(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+
+                };
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View();
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CoreApplication1/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreApplication1.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public String RoleName { get; set; }
+
+    }
+}
